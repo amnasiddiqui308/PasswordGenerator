45826398750e96dfff710d85f33789f626215fe9
unknown <amnasiddiqui308@gmail.com>
2024-06-21 21:19:20 +0500

    Password is now being generated, but in an order.

--- main.cpp
+++ main.cpp
@@ -37,50 +37,94 @@ void getSpecifications(bool& upperCase, bool& lowerCase, bool& number, bool& sym
 	cin >> symbol;
 }
 
-char upperCase() {
-	srand(time(0));
+char UpperCase(bool uc) {
 	char letter = '\0';
-
+	
 	letter = rand() % (90 - 65 + 1) + 65;
+	letter *= uc; // checks if user chose uppercase or not. Letter becomes zero if he didn't chose this specification.
 
 	return letter;
 }
 
-char lowerCase() {
-	srand(time(0));
+char LowerCase(bool lc) {
 	char letter = '\0';
 
 	letter = rand() % (122 - 97 + 1) + 97;
+	letter *= lc; // checks if user chose lowercase or not.
 
 	return letter;
 }
 
-char number() {
-	srand(time(0));
+char Number(bool n) {
 	char number = '\0';
 
 	number = rand() % (57 - 48 + 1) + 48;
+	number *= n; // checks if user chose number or not.
 
 	return number;
 }
 
-char symbol(char symbols[]) {
+char Symbol(char symbols[], bool s) {
 	char symbol = rand() % 32;
 
-	return symbols[symbol];
+	return (symbols[symbol] * s); // (*s) checks if user chose symbol or not.
 }
 
-//string generatePassword(int length, bool upperCase, bool lowerCase, bool number, bool symbol) {
-//	string password = "";
-//
-//
-//}
+string generatePassword(int length, bool upperCase, bool lowerCase, bool number, bool symbol, char symbols[]) {
+	string password = "";
+	int i = 0;
+	
+	while (1) {
+		char value = 0;
+
+		value = UpperCase(upperCase);
+		if (value != 0) {
+			password += value;
+			i++;
+
+			if (i == length)
+				break;
+		}
+
+		value = LowerCase(lowerCase);
+		if (value != 0) {
+			password += value;
+			i++;
+
+			if (i == length)
+				break;
+		}
+
+		value = Number(number);
+		if (value != 0) {
+			password += value;
+			i++;
+
+			if (i == length)
+				break;
+		}
+
+		value = Symbol(symbols, symbol);
+		if (value != 0) {
+			password += value;
+			i++;
+
+			if (i == length)
+				break;
+		}
+	}
+
+	return password;
+}
 
 int main() {
+	srand(time(0));
 	int length = 0;
 	bool upperCase = 0, lowerCase = 0, number = 0, symbol = 0;
 	char symbols[32] = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', '\\', ':', ';', '"', '\'', '<', ',', '>', '.', '?', '/' , '`', '~' };
 
 	length = getLength();
-	cout << symbols[19];
+	getSpecifications(upperCase, lowerCase, number, symbol);
+
+	cout << generatePassword(length, upperCase, lowerCase, number, symbol, symbols) << endl;
 }
